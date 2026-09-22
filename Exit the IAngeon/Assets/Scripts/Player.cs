aaa255f6e4c3bb61247d3c93cceb6f2c330a07fb
Marcusius334 <al436953@uji.es>
2026-09-22 18:15:09 +0200

    Todo funciona

--- Exit the IAngeon/Assets/Scripts/Player.cs
+++ Exit the IAngeon/Assets/Scripts/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
 
     private Vector2 movement;
     private SpriteRenderer spriteRenderer;
+    public SceneLoader sceneLoader;
 
     void Start()
     {
@@ -37,5 +38,17 @@ public class Player : MonoBehaviour
         float currentSpeed = isRunning ? runSpeed : speed;
 
         transform.Translate(movement * currentSpeed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Detectar las colisiones con los enemigos y cosas
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            sceneLoader.LoadScene("Death");
+        }else if (collision.gameObject.CompareTag("Exit"))
+        {
+            sceneLoader.LoadScene("Victory");
+        }
     }
 }
